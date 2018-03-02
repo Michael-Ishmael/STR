5fdab7481a08365d709545256c0cd8d241844ca1
Michael Ishmael <michaelishmael1976@gmail.com>
2018-03-02 01:14:30 +0000

    Services page

---
+++ str_html/pug/temp.js
@@ -0,0 +1,46 @@
+var abc = [
+    {
+        id: 'skyrocket',
+        title: 'Helping DHL Express  Skyrocket Their Sales',
+        link: '#',
+        image: 'img/success-abc.jpg'
+    },
+    {
+        id: 'abc',
+        title: 'abc',
+        link: '#',
+        image: 'img/success-abc.jpg'
+    },
+    {
+        id: 'abc',
+        title: 'abc',
+        link: '#',
+        image: 'img/success-abc.jpg'
+    },
+    {
+        id: 'abc',
+        title: 'abc',
+        link: '#',
+        image: 'img/success-abc.jpg'
+    },
+    {
+        id: 'abc',
+        title: 'abc',
+        link: '#',
+        image: 'img/success-abc.jpg'
+    },
+    {
+        id: 'abc',
+        title: 'abc',
+        link: '#',
+        image: 'img/success-abc.jpg'
+    },
+    {
+        id: 'abc',
+        title: 'abc',
+        link: '#',
+        image: 'img/success-abc.jpg'
+    }
+
+];
+
